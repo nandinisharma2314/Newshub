53ed0ca47c3f592f0a4610999d6b98b56769ce30
nandinisharma2314 <nandinisharma142004@gmail.com>
2026-05-18 19:40:01 +0530

    fix news field

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import News from "./components/News";
 
 const App = () => {
   const pageSize = 5;
-  const apiKey = import.meta.env.VITE_APP_NEWS_API;
+  const apiKey = import.meta.env.VITE_APP_GNEWS_API;
   console.log(apiKey);
   const [progress, setProgress] = useState(0);
 
@@ -17,14 +17,14 @@ const App = () => {
       <LoadingBar height={3} color="#f11946" progress={progress} />
 
       <Routes>
-        <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="us" category="general" />} />
-        <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="us" category="business" />} />
-        <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="us" category="entertainment" />} />
-        <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="us" category="general" />} />
-        <Route path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="us" category="health" />} />
-        <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="us" category="science" />} />
-        <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="us" category="sports" />} />
-        <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="us" category="technology" />} />
+        <Route path="/" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+        <Route path="/business" element={<News setProgress={setProgress} apiKey={apiKey} key="business" pageSize={pageSize} country="in" category="business" />} />
+        <Route path="/entertainment" element={<News setProgress={setProgress} apiKey={apiKey} key="entertainment" pageSize={pageSize} country="" in="entertainment" />} />
+        <Route path="/general" element={<News setProgress={setProgress} apiKey={apiKey} key="general" pageSize={pageSize} country="in" category="general" />} />
+        <Route path="/health" element={<News setProgress={setProgress} apiKey={apiKey} key="health" pageSize={pageSize} country="in" category="health" />} />
+        <Route path="/science" element={<News setProgress={setProgress} apiKey={apiKey} key="science" pageSize={pageSize} country="in" category="science" />} />
+        <Route path="/sports" element={<News setProgress={setProgress} apiKey={apiKey} key="sports" pageSize={pageSize} country="in" category="sports" />} />
+        <Route path="/technology" element={<News setProgress={setProgress} apiKey={apiKey} key="technology" pageSize={pageSize} country="in" category="technology" />} />
       </Routes>
     </>
   );
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -12,14 +12,19 @@ const News = (props) => {
     const updateNews = async () => {
         try {
             props.setProgress(10);
-            const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${page}&pageSize=${props.pageSize}`;
+            const url = `https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&country=${props.country}&max=${props.pageSize}&page=${page}&apikey=${props.apiKey}`;
             setLoading(true);
             const response = await fetch(url);
             props.setProgress(30);
             const parsedData = await response.json();
             props.setProgress(70);
 
-            if (parsedData.status !== "ok") return console.error(parsedData), setArticles([]), setLoading(false);
+            if (!parsedData.articles) {
+                console.error(parsedData);
+                setArticles([]);
+                setLoading(false);
+                return;
+            }
 
             setArticles(parsedData.articles || []);
             setTotalResults(parsedData.totalResults || 0);
@@ -39,12 +44,13 @@ const News = (props) => {
             const nextPage = page + 1;
             setPage(nextPage);
 
-            const url = `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=${props.apiKey}&page=${nextPage}&pageSize=${props.pageSize}`;
+            const url = `https://gnews.io/api/v4/top-headlines?category=${props.category}&lang=en&country=${props.country}&max=${props.pageSize}&page=${nextPage}&apikey=${props.apiKey}`;
 
             const response = await fetch(url);
             const parsedData = await response.json();
 
-            if (parsedData.status !== "ok") return;
+            if (!parsedData.articles) return;
+
 
             setArticles((prevArticles) => prevArticles.concat(parsedData.articles || []));
             setTotalResults(parsedData.totalResults || 0);
@@ -62,7 +68,7 @@ const News = (props) => {
             <InfiniteScroll dataLength={articles?.length || 0} next={fetchMoreData} hasMore={articles.length !== totalResults} loader={<Spinner />}>
                 <div className="container">
                     <div className="row">
-                        {articles?.map((element) => <div className="col-md-4" key={element.url}><NewsItem title={element.title || ""} description={element.description || ""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source?.name} /></div>)}
+                        {articles?.map((element) => <div className="col-md-4" key={element.url}><NewsItem title={element.title || ""} description={element.description || ""} imageUrl={element.image} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source?.name} /></div>)}
                     </div>
                 </div>
             </InfiniteScroll>
@@ -70,7 +76,7 @@ const News = (props) => {
     );
 };
 
-News.defaultProps = { country: "us", pageSize: 8, category: "general" };
+News.defaultProps = { country: "in", pageSize: 8, category: "general" };
 News.propTypes = { country: PropTypes.string, pageSize: PropTypes.number, category: PropTypes.string };
 
 export default News;
